0ea5a4eeea57a7f9ba2dfa34a45e54bd622a3aa0
sabrina-reis1 <101716378+sabrina-reis1@users.noreply.github.com>
2022-11-15 19:49:30 -0300

    Delete MvcLabManager directory

--- MvcLabManager/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace MvcLabManager.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
